1eecf1f4d63cf99c3f101b0704d9830926486aa3
ZhangJY <ZhangJY@atguigu.com>
2020-10-16 16:19:13 +0800

    hot-fix commit

--- Hello/src/main/java/Hello.java
+++ Hello/src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public String sayHello(String name){
+        System.out.println("Hello Git");
+        System.out.println("hot-fix");
         return "Hello" + name + "!";
     }
 }
